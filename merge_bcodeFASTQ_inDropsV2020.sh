0ba2703930e5179ee9cfa0d4b9844dabcebf6499
danwagnerlab <38510695+wagnerde@users.noreply.github.com>
2023-08-18 15:31:52 -0700

    Update merge_bcodeFASTQ_inDropsV2020.sh

--- merge_bcodeFASTQ_inDropsV2020.sh
+++ merge_bcodeFASTQ_inDropsV2020.sh
@@ -12,18 +12,13 @@
 # R3 = biological read
 # I1 = library index (unnecessary post-demultiplexing)
 #
-# Usage: 
-# 'source merge_bcodeFASTQ_inDropsV2020.sh scNAS001'
-# This will combine metadata reads scNAS001_R2.fastq.gz and scNAS001_R1.fastq.gz and write the file scNAS001_R1R2.fastq.gz
+# Usage:
+# The script will automatically detect sets of FASTQ files in the current working directory with shared
+# base filenames and writes a merged '*R1R2.fastq.gz' file for each.
 # 
-# 'source merge_bcodeFASTQ_inDropsV2020.sh scNAS001 scNAS002 scNAS003'
-# This will perform multiple conversions for each set of FASTQ files
-
 # CODE:
-# input the user-specified FASTQ file basenames as an array
-#input=( "$@" )
 
-for filename in NAS019*R1.fastq.gz; do
+for filename in *R1.fastq.gz; do
 
     # get the base filename for each set of reads
     basename=$(echo $filename | sed 's/R1.fastq.gz//')
@@ -38,19 +33,4 @@ for filename in NAS019*R1.fastq.gz; do
 
 done
 
-# loop through each set of FASTQ files
-#for bname in ${input[@]}
-#do    
-#    
-#    echo Combining barcodes for $bname #
-#	
-#	# concatenate R2 and R1 sequences for each read; revcomp R2
-#	seqkit concat <(seqkit seq --reverse --complement --seq-type 'dna' ${bname}_R2.fastq.gz) ${bname}_R1.fastq.gz \
-#    	--out-file ${bname}_R1R2.fastq.gz \
-#    	--line-width 0 \
-#    	--threads 16
-#
-#	# old version lacking the reverse complement step
-#	#paste <(zcat < ${bname}_R2.fastq.gz) <(zcat < ${bname}_R1.fastq.gz) | paste - - - - | awk -F'\t' '{OFS="\n"; print $1,$3$4,$5,$7$8}' | gzip - > ${bname}_R1R2.fastq.gz
-#
-#done
+
